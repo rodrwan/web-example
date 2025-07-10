be83de9edb806c8e562d263dff53ba2edff97c50
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2025-07-09 20:51:21 -0400

    add hero

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log/slog"
+	"net/http"
 	"os"
 
 	"web-example/internal/ui"
@@ -8,8 +12,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//go:generate ./...
+
+//go:embed static
+var staticFS embed.FS
+
 func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})))
+
 	s := gin.Default()
+
+	// Serve static files
+	staticSubFS, err := fs.Sub(staticFS, "static")
+	if err != nil {
+		slog.Error("Error configurando archivos estáticos", "error", err)
+		panic(err)
+	}
+	s.StaticFS("/static", http.FS(staticSubFS))
 	s.GET("/", func(c *gin.Context) {
 		component := ui.Layout()
 		component.Render(c.Request.Context(), c.Writer)
